86aa8a228e7944cfaf5b298aa30a95b9ebd5c7c8
Edvins Antonovs <ummahusla@gmail.com>
2016-07-26 21:42:30 +0100

    Initial commit

---
+++ js/contacts.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    contactsNamespace.initialize();
+});
+
+(function () {
+    this.contactsNamespace = this.contactsNamespace || {};
+    var ns = this.contactsNamespace;
+    var currentRecord;
+
+    ns.initialize = function () {
+
+    };
+})();
